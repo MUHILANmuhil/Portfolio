06744b07631c0ef843b1e67561da641edfbee09f
MUHILANmuhil <muhilanvenkat@gmail.com>
2022-08-11 20:04:23 +0530

    Updates

---
+++ precache-manifest.88ae8a832e1fd6b5fe0844c03663f305.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ee55d0837418cb51af",
+    "url": "/portfolio/static/css/main.6fefe34a.chunk.css"
+  },
+  {
+    "revision": "42ee55d0837418cb51af",
+    "url": "/portfolio/static/js/main.42ee55d0.chunk.js"
+  },
+  {
+    "revision": "f643efff6e433eee2960",
+    "url": "/portfolio/static/js/1.f643efff.chunk.js"
+  },
+  {
+    "revision": "cdc596e7a94ba479b111",
+    "url": "/portfolio/static/js/2.cdc596e7.chunk.js"
+  },
+  {
+    "revision": "03e97a3ecc2479f2dcd6",
+    "url": "/portfolio/static/js/runtime~main.03e97a3e.js"
+  },
+  {
+    "revision": "9fbbb0252f6bb4047f7c16995cbe6db8",
+    "url": "/portfolio/static/media/pic3.9fbbb025.svg"
+  },
+  {
+    "revision": "69ba5853ada95334ba30ff3ac9682164",
+    "url": "/portfolio/static/media/contact_bpic.69ba5853.png"
+  },
+  {
+    "revision": "fee78640ee734d8b234dd34d0837003e",
+    "url": "/portfolio/index.html"
+  }
+];
